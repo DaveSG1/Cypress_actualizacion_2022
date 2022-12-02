b9567c62f48999dd2f98db94a009e3252697de56
DaveSG1 <david_serna_83@hotmail.com>
2022-12-02 14:31:09 +0100

    añadimos should a los botones

--- cypress/e2e/seccion_3/reto_for_y_each.cy.js
+++ cypress/e2e/seccion_3/reto_for_y_each.cy.js
@@ -19,13 +19,13 @@ describe("Reto bucles For y each", () => {
         for(let x=0; x<=3; x++){
             cy.get(".hrefch").eq(x).click() //le digo que coja la etiqueta del nombre de cada móvil y con el eq() de digo que en esa vuelta seleccione el movil que tenga el valor de x (por lo que en la primera vuelta cogerá el 0 ó primer móvil, luego el 1 ó segundo móvil, etc)
             cy.wait(1500)
-            cy.get('.col-sm-12 > .btn').click() //le digo que, abierta la ventana del móvil que toque en esa vuelta, le de al botón de añadir a la cesta
+            cy.get('.col-sm-12 > .btn').should("be.visible").click() //le digo que, abierta la ventana del móvil que toque en esa vuelta, le de al botón de añadir a la cesta
             cy.wait(1500) 
-            cy.get('.active > .nav-link').click() // le digo que, una vez añadido a la cesta, haga click en el botón Home
+            cy.get('.active > .nav-link').should("be.visible").click() // le digo que, una vez añadido a la cesta, haga click en el botón Home
             cy.wait(1500) 
         }
 
-        cy.get(':nth-child(4) > .nav-link').click() // le digo que, una vez terminado el bucle y añadidos todos los móviles a la cesta, haga click en la cesta para comprobar que todos los móviles indicados (4) se han añadido
+        cy.get(':nth-child(4) > .nav-link').should("be.visible").click() // le digo que, una vez terminado el bucle y añadidos todos los móviles a la cesta, haga click en la cesta para comprobar que todos los móviles indicados (4) se han añadido
                    
     })
     
