277aa3c505623f332c6373bfc40bef8325c1ce07
DaveSG1 <david_serna_83@hotmail.com>
2022-12-14 14:29:01 +0100

    tip fixture con archivo excel en vez de json

---
+++ cypress/e2e/seccion_7_Fixtures/fixture_desde_Excel.cy.js
@@ -0,0 +1,6 @@
+//Si los datos los tengo en un Excel en vez de en un JSON
+//visito la página web:      beautifytools.com/excel-to-json-converter.php
+//pulso Browse, busco mi archivo excel y lo añado, me lo transforma y le doy a Download
+//y lo añado a la carpeta fixtures de mi proyecto (lo pone como .txt), 
+//le quito las primeras llaves para que quede corchetes que engloban a llaves (array de objetos)
+//Lo último, en la carpeta fixtures renombro el archivo con el nombre que quiera y en vez de .txt ponerle .json
